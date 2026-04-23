e96dbd419a39e52094c5117d59eb4b33094a452d
Hoyt <hoythan@gmail.com>
2026-04-24 01:53:08 +0800

    fix(tvmaze): 移除测试中不存在的 apiKey 字段引用

--- provider/tvmaze/tvmaze_test.go
+++ provider/tvmaze/tvmaze_test.go
@@ -2,64 +2,39 @@ package tvmaze
 
 import (
 	"inscurascraper/provider/internal/testkit"
-	"os"
 	"testing"
 )
 
-// TVMaze public API does not require authentication.
-// Set env IS_TVMAZE_API_KEY if needed for premium features.
-var apiKey = os.Getenv("IS_TVMAZE_API_KEY")
-
 func TestTVMaze_GetMovieInfoByID(t *testing.T) {
-	testkit.Test(t, func() *TVMaze {
-		res := New()
-		res.apiKey = apiKey
-		return res
-	}, []string{
+	testkit.Test(t, New, []string{
 		"169", // Breaking Bad
 		"82",  // Game of Thrones
 	})
 }
 
 func TestTVMaze_GetMovieInfoByURL(t *testing.T) {
-	testkit.Test(t, func() *TVMaze {
-		res := New()
-		res.apiKey = apiKey
-		return res
-	}, []string{
+	testkit.Test(t, New, []string{
 		"https://www.tvmaze.com/shows/169/breaking-bad",
 		"https://www.tvmaze.com/shows/82/game-of-thrones",
 	})
 }
 
 func TestTVMaze_SearchMovie(t *testing.T) {
-	testkit.Test(t, func() *TVMaze {
-		res := New()
-		res.apiKey = apiKey
-		return res
-	}, []string{
+	testkit.Test(t, New, []string{
 		"Breaking Bad",
 		"Game of Thrones",
 	})
 }
 
 func TestTVMaze_GetActorInfoByID(t *testing.T) {
-	testkit.Test(t, func() *TVMaze {
-		res := New()
-		res.apiKey = apiKey
-		return res
-	}, []string{
+	testkit.Test(t, New, []string{
 		"14245", // Bryan Cranston
 		"1",     // Mike Vogel
 	})
 }
 
 func TestTVMaze_SearchActor(t *testing.T) {
-	testkit.Test(t, func() *TVMaze {
-		res := New()
-		res.apiKey = apiKey
-		return res
-	}, []string{
+	testkit.Test(t, New, []string{
 		"Bryan Cranston",
 	})
 }
